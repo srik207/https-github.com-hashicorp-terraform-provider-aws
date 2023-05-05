406ad78f584122cd4ced4879b3ae4dda59545f07
Daniel Scain Farenzena <d.scain.farenzena@gmail.com>
2023-05-05 17:00:03 +0200

    Add resource "aws_redshiftdata_batch_statement"

--- internal/service/redshiftdata/service_package_gen.go
+++ internal/service/redshiftdata/service_package_gen.go
@@ -29,7 +29,11 @@ func (p *servicePackage) SDKDataSources(ctx context.Context) []*types.ServicePac
 func (p *servicePackage) SDKResources(ctx context.Context) []*types.ServicePackageSDKResource {
 	return []*types.ServicePackageSDKResource{
 		{
-			Factory:  resourceStatement,
+			Factory:  ResourceBatchStatement,
+			TypeName: "aws_redshiftdata_batch_statement",
+		},
+		{
+			Factory:  ResourceStatement,
 			TypeName: "aws_redshiftdata_statement",
 		},
 	}
