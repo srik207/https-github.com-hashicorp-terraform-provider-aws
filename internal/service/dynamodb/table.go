27b40573d9db84c701fc2db7facdf38593a21817
Josh Myers <joshuajmyers@gmail.com>
2023-05-09 11:11:31 +0100

    Fix DDB table Amazon Owned KMS key with replicas
    Some time ago Amazon changed the default DDB encryption behaviour to use
    a key 'Managed by DynamoDB' (it lives in the DDB internal account) if
    setting `server_side_encryption.enabled = false`
    
    The current update table logic, if using replicas, assumes you are using
    a CMK. If this is not the case, an update table request is triggered
    with an empty `kMSMasterKeyId`[1] and AWS API throws the below error.
    
    This commit fixes the logic to only update replicas if there is a CMK
    used.
    
    [1]
    ```
    │ Error: updating DynamoDB Table (BADGERS) SSE: ValidationException: 4 validation errors detected: Value '' at 'replicaUpdates.1.member.update.kMSMasterKeyId' failed to satisfy constraint: Member must have length greater than or equal to 1; Value '' at 'replicaUpdates.2.member.update.kMSMasterKeyId' failed to satisfy constraint: Member must have length greater than or equal to 1; Value '' at 'replicaUpdates.3.member.update.kMSMasterKeyId' failed to satisfy constraint: Member must have length greater than or equal to 1; Value '' at 'replicaUpdates.4.member.update.kMSMasterKeyId' failed to satisfy constraint: Member must have length greater than or equal to 1
        ```
    
    Fixes https://github.com/hashicorp/terraform-provider-aws/issues/31153

--- internal/service/dynamodb/table.go
+++ internal/service/dynamodb/table.go
@@ -908,7 +908,8 @@ func resourceTableUpdate(ctx context.Context, d *schema.ResourceData, meta inter
 	}
 
 	if d.HasChange("server_side_encryption") {
-		if replicas := d.Get("replica").(*schema.Set); replicas.Len() > 0 {
+		KMSMasterKeyId := expandEncryptAtRestOptions(d.Get("server_side_encryption").([]interface{})).KMSMasterKeyId
+		if replicas := d.Get("replica").(*schema.Set); replicas.Len() > 0 && KMSMasterKeyId != nil {
 			log.Printf("[DEBUG] Using SSE update on replicas")
 			var replicaInputs []*dynamodb.ReplicationGroupUpdate
 			var replicaRegions []string
