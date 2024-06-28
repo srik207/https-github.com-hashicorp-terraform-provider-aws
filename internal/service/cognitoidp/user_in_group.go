ddd2df1e69d109064ad65825f0e54da5f9b40829
Daniel Quackenbush <25692880+danquack@users.noreply.github.com>
2024-06-28 10:29:03 -0400

    :broom:

--- internal/service/cognitoidp/user_in_group.go
+++ internal/service/cognitoidp/user_in_group.go
@@ -14,7 +14,6 @@ import (
 	awstypes "github.com/aws/aws-sdk-go-v2/service/cognitoidentityprovider/types"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
-
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
